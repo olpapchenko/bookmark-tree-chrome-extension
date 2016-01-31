3a1dd3804deab27b2809754b48a27451295527e5
Oleksandr <rocketmannot@gmail.com>
2016-01-31 20:20:30 +0200

    [+] edit popup added

--- js/content/content.js
+++ js/content/content.js
@@ -1,8 +1,37 @@
 var commentController = new CommentController(),
-    linkController = new LinkController();
+    linkController = new LinkController(),
+    EDIT_MODE_POPUP_HTML  = "<div id='bookmark_tree_instructions' class='extensionText'>You are at the edit mode, to exit press ESC.</div>";
 
+function createEditModeInstructionPopup() {
+    var HIDE_POPUP_TIMEOUT = 400000;
+    function hidePopup() {
+        console.log(document.querySelector("#bookmark_tree_instructions"));
+        document.querySelector("#bookmark_tree_instructions").className = " transperent";
+    }
+
+    var element = document.createElement("div");
+        element.innerHTML = EDIT_MODE_POPUP_HTML,
+        removeSign = document.createElement("div");
+
+    removeSign.className = "removeContainer";
+    removeSign.style.background = "url('" + chrome.extension.getURL("/images/cross.png") +"') no-repeat";
+    removeSign.style.backgroundSize = "10px";
+
+    removeSign.addEventListener("click", function () {
+        console.log("clicked");
+        hidePopup();
+    });
+
+    element.childNodes[0].appendChild(removeSign);
+    document.body.appendChild(element);
+    setTimeout(function () {
+        hidePopup();
+    }, HIDE_POPUP_TIMEOUT);
+}
+//Listeners section
 chrome.runtime.onMessage.addListener(function (message) {
     if(message.type == MESSAGE_TYPES.MARK_MODE_START) {
+        createEditModeInstructionPopup();
         markCurrentSelection();
         enableMarker(true);
         document.body.style.cursor = "url(" +  chrome.extension.getURL('/images/mark.png') + ") 0 26, auto";
@@ -21,6 +50,7 @@ chrome.runtime.onMessage.addListener(function (message) {
     }
 
     if(message.type == MESSAGE_TYPES.COMMENT_MODE_START) {
+        createEditModeInstructionPopup();
         document.body.style.cursor = "url(" +  chrome.extension.getURL('/images/comment.png') + ") 0 26, auto";
         commentController.enable(true);
     }
@@ -34,6 +64,7 @@ chrome.runtime.onMessage.addListener(function (message) {
     }
 
     if(message.type == MESSAGE_TYPES.BOOKMARK_TREE_BUILDER_START) {
+        createEditModeInstructionPopup();
         linkController.enable(true);
         document.body.style.cursor = "url(" +  chrome.extension.getURL('/images/bookmark.png') + ") 0 26, auto";
     }
