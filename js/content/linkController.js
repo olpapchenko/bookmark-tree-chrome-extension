54dd2326a1f9a8281a0d9e6601054d027a144154
Oleksandr <rocketmannot@gmail.com>
2016-01-31 21:19:38 +0200

    [+] link sign for link boxes

--- js/content/linkController.js
+++ js/content/linkController.js
@@ -1,7 +1,7 @@
 function LinkController() {
     CommentControllerBase.apply(this, arguments);
     this.getBodyMarkUp = function () {
-        var LINK_MARK_UP = "<div class='linkWrapper'><div class='commentContainer'></div></div>";
+        var LINK_MARK_UP = "<div class='linkWrapper'><div class='commentContainer'><img src=" + chrome.extension.getURL("/images/bookmark.png") + "></div></div>";
         return LINK_MARK_UP;
     }
 
