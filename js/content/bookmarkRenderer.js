7d56d1e9fe617b93aa0650e248e15dee88c302b4
Oleksandr <rocketmannot@gmail.com>
2016-03-14 14:28:40 +0200

    [+] do not render entities if in preferences they are prohibited

--- js/content/bookmarkRenderer.js
+++ js/content/bookmarkRenderer.js
@@ -1,48 +1,64 @@
  bookmarkRenderer = {
      renderBookmark: function (bookmark) {
          var _this = this;
-         var compare = function (entity1, entity2) {return entity1.order - entity2.order;}
 
-         var entities = {
-             markers: bookmark.markers,
-             comments: bookmark.comments,
-             links: bookmark.links
-         }
-         var allEntities = [];
-
-         Object.keys(entities).forEach(function (key){
-             if(!entities[key]) {
+         preferencesService.get(preferencesService.MARKS_ENABLED).then(function (preference) {
+             if(!preference[preferencesService.EXTENSION_ENABLED].value) {
                  return;
              }
-             var res = entities[key].map(function (entity) {
-                 entity.type = key
-                 return entity;
-             });
+             var compare = function (entity1, entity2) {return entity1.order - entity2.order;}
 
-             if(res.length > 0) {
-                 allEntities =  allEntities.concat(res);
+             var entities = {
+                 markers: bookmark.markers,
+                 comments: bookmark.comments,
+                 links: bookmark.links
              }
-        });
-         var time = 200;
+             var allEntities = [];
 
-         allEntities.sort(compare).forEach(function (entity) {
-            if(entity.type == "markers")  {
-                setTimeout(function () {
-                    _this.renderMarker(entity, bookmark.isOwner);
-                }, time);
-                time += 200;
-            } else if(entity.type == "comments") {
-                setTimeout(function () {
-                    _this.renderComment(entity, bookmark.isOwner);
-                }, time);
-                time += 200;
-            } else if (entity.type == "links") {
-                setTimeout(function () {
-                    _this.renderLink(entity, bookmark.isOwner);
-                }, time);
-                time += 200;
-            }
+             Object.keys(entities).forEach(function (key){
+                 if(!entities[key]) {
+                     return;
+                 }
+                 var res = entities[key].map(function (entity) {
+                     entity.type = key
+                     return entity;
+                 });
+
+                 if(res.length > 0) {
+                     allEntities =  allEntities.concat(res);
+                 }
+             });
+             var time = 200;
+
+             allEntities.sort(compare).forEach(function (entity) {
+                 if(entity.type == "markers")  {
+                     setTimeout(function () {
+                         if(!preference[preferencesService.MARKS_ENABLED].value){
+                             return;
+                         }
+                         _this.renderMarker(entity, bookmark.isOwner);
+                     }, time);
+                     time += 200;
+                 } else if(entity.type == "comments") {
+                     setTimeout(function () {
+                         if(!preference[preferencesService.COMMENTS_ENABLED].value){
+                             return;
+                         }
+                         _this.renderComment(entity, bookmark.isOwner);
+                     }, time);
+                     time += 200;
+                 } else if (entity.type == "links") {
+                     setTimeout(function () {
+                         if(!preference[preferencesService.BOOKMARK_LINKS_ENABLED].value){
+                             return;
+                         }
+                         _this.renderLink(entity, bookmark.isOwner);
+                     }, time);
+                     time += 200;
+                 }
+             });
          });
+
      },
 
      renderMarker: function (marker, isOwner) {
