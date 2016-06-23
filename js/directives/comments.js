d8c005caa64883b40a074b97c91232bbe4e449bc
Oleksandr <rocketmannot@gmail.com>
2016-06-23 21:28:47 +0300

    do not display  empty entities

--- js/directives/comments.js
+++ js/directives/comments.js
@@ -7,6 +7,9 @@ angular.module("app").directive("comments",["bookmarkService", function (bookmar
                 $scope.$apply(function () {
                     console.log(bookmark);
                     $scope.bookmark = bookmark;
+                    $scope.comments = bookmark.comments.filter(function (comment) {
+                        return comment.display;
+                    });
                 });
             });
 
@@ -15,8 +18,10 @@ angular.module("app").directive("comments",["bookmarkService", function (bookmar
                 setTimeout(function () {
                     bookmarkService.getCurrentBookmark().then(function(bookmark){
                         $scope.$apply(function () {
-                            console.log(bookmark);
                             $scope.bookmark = bookmark;
+                            $scope.comments = bookmark.comments.filter(function (comment) {
+                                return comment.display;
+                            });
                         });
                     });
                 }, 100);
